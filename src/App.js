2fba71db0479ea2e106095dc68a4aa596d4f4c46
claudio <cjkrzckotowski@gmail.com>
2024-09-11 07:14:09 -0300

    criando componentes Tipografia e CampoTexto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import { CampoTexto } from "./componenentes/CampoTexto/CampoTexto";
 import { Card } from "./componenentes/Card/Card";
 import { Estilos } from "./componenentes/EstilosGlobais/Estilos";
 import { ProvedorTema } from "./componenentes/ProvedorTema/ProvedorTema";
+import { Tipografia } from "./componenentes/Tipografia/Tipografia";
 
 function App() {
   return (
     <ProvedorTema>
       <Estilos />
       <Card>
-        <h1>Freelando</h1>
-      </Card> 
+        <Tipografia variante="h1" componente="h1">
+          Freelando
+        </Tipografia>
+        <Tipografia variante="body" componente="body">
+          Crie seu perfil gratuitamente para começar a trabalhar com os melhores
+          freelancers. Em seguida, você poderá dar mais detalhes sobre suas
+          demandas e sobre sua forma de trabalho.
+        </Tipografia>
+        <CampoTexto titulo="Nome Completo" />
+      </Card>
     </ProvedorTema>
   );
 }
 
 export default App;
- 
